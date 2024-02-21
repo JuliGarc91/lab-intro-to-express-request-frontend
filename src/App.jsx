b2bdbe5dbd9cf9ba1225d836c29028ac91787559
juligarc91 <julissagarcia@pursuit.org>
2024-02-21 17:29:34 -0500

    imported data and implemented useEffect and useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+
 
 const App = () => {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(()=>{
+    fetch("http://localhost:8888/pokemon")
+    .then((res) => res.json())
+    .then((data) => setPokemon(data))
+  }, [])
+  // console.log(pokemon)
   return (
     <div>
-      <h1>Pokemon Frontend</h1>
+      {
+        pokemon.length > 0 &&
+        pokemon.map((singlePoke, index)=>(
+          <div key={index}>
+          <img src={singlePoke.img} alt={singlePoke.name}/>
+          </div>
+        ))
+      }
     </div>
   )
 }
 
 export default App
+
+/* Frontend:
+Fork & Clone the Lab Intro to Express Request Frontend.
+
+Setup your React environment
+
+Make a fetch call to your backend, in your App.jsx, that GETS all of the pokemon.
+
+For each pokemon, display the image and any information of your choice on the view.
+*/
