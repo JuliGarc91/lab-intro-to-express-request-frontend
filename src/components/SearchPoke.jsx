bec92922f70219320b0277b40cc81c75f19f8536
juligarc91 <julissagarcia@pursuit.org>
2024-02-21 19:39:58 -0500

    added search route

--- src/components/SearchPoke.jsx
+++ src/components/SearchPoke.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
 
 const SearchPoke = () => {
+    const [pokemonName, setPokemonName]= useState('');
+    useEffect(()=>{
+        fetch(`http://localhost:8888/pokemon/search?name=${pokemonName}`)
+    })
   return (
     <div>SearchPoke</div>
   )
